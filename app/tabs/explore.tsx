7cefb72e2208645b8494623bbffc56fa93277738
AnfuGo <rafael.oliveira789whd@gmail.com>
2025-11-12 18:53:00 -0300

    nadademais

--- app/tabs/explore.tsx
+++ app/tabs/explore.tsx
@@ -320,7 +320,7 @@ const executarAcaoDeSeguranca = () => {
       </ThemedView>
 
       <ThemedView style={[styles.titleContainer, { justifyContent: "center" }]}>
-        <ThemedText type="default" style={styles.color}>Área Traseira de aquecimento</ThemedText>
+        <ThemedText type="default" style={styles.color}>Área Frontal de aquecimento</ThemedText>
       </ThemedView>
       <View style={styles.titleContainer}>
         <Pressable onPress={TrocaCor} style={[styles.button, { backgroundColor: bt1Color }]}>
@@ -338,7 +338,7 @@ const executarAcaoDeSeguranca = () => {
       </View>
 
       <ThemedView style={[styles.titleContainer, { justifyContent: "center" }]}>
-        <ThemedText type="default" style={styles.color}>Área lateral esquerda de aquecimento</ThemedText>
+        <ThemedText type="default" style={styles.color}>Área lateral direita de aquecimento</ThemedText>
       </ThemedView>
       <View style={styles.titleContainer}>
         <Pressable onPress={TrocaCor2} style={[styles.button, { backgroundColor: bt2Color }]}>
@@ -356,7 +356,7 @@ const executarAcaoDeSeguranca = () => {
       </View>
 
       <ThemedView style={[styles.titleContainer, { justifyContent: "center" }]}>
-        <ThemedText type="default" style={styles.color}>Área lateral direita de aquecimento</ThemedText>
+        <ThemedText type="default" style={styles.color}>Área lateral esquerda de aquecimento</ThemedText>
       </ThemedView>
       <View style={styles.titleContainer}>
         <Pressable onPress={TrocaCor3} style={[styles.button, { backgroundColor: bt3Color }]}>
